58e44b42830f462b4357634b9a7d12967f1c8c9c
Torrekie <me@torrekie.dev>
2022-01-10 01:48:26 +0800

    libunc

---
+++ src/libSystem/libunc/stub.c
@@ -0,0 +1,4 @@
+// Apple didn't fully opened UserNotification
+// STUB AGAIN!!!!
+const unsigned char uncVersionString[] = "@(#)PROGRAM:unc  PROJECT:UserNotification-30";
+const double uncVersionNumber = 30;
